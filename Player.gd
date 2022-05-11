f1633ee694feefe57366f2df460f5a73fe35efd5
Jack Mordaunt <jackmordaunt.dev@gmail.com>
2022-05-11 17:55:44 +0800

    flappy: generate barriers
    Signed-off-by: Jack Mordaunt <jackmordaunt.dev@gmail.com>

--- Player.gd
+++ Player.gd
@@ -2,32 +2,25 @@ extends Actor
 
 onready var sprite = $AnimatedSprite
 
-const KILL_CEIL = -300
-const KILL_FLOOR = 300
-
 export var cam_speed = 250
 
 var direction = Vector2(0, 0);
 var acceleration = 1
 
 func _physics_process(dt) -> void:
-	process_kill_barriers()
-	direction = get_direction()
-	process_input(dt)
-	self.velocity = move_and_slide(self.velocity, FLOOR_STD)
 	acceleration += 0.001
+	self.position.x += cam_speed * acceleration * dt
 
 func _process(dt):
+	direction = get_direction()
+	process_input(dt)
+
+	self.velocity = move_and_slide(self.velocity, FLOOR_STD)
+
 	if direction.y == 1.0:
 		sprite.play("flap-up")
 	if direction.y == -1.0:
 		sprite.play("flap-down")
-	if direction.x == -1.0:
-		sprite.flip_h = true
-	if direction.x == 1.0:
-		sprite.flip_h = false
-
-	self.position.x += cam_speed * dt * acceleration
 
 #gets input key from the player
 func get_direction() -> Vector2:
@@ -57,10 +50,4 @@ func process_input(dt) -> void:
 	var jump_interupt = Input.is_action_just_released("up") and self.velocity.y < 0.0
 	self.velocity = calc_velocity(dt, self.velocity, direction, speed, jump_interupt)
 
-# if we hit a ceiling or floor, tp actor to origin.
-func process_kill_barriers():
-	if position.y > KILL_FLOOR:
-		position = Vector2(0, 0)
-	if position.y < KILL_CEIL:
-		position = Vector2(0, 0)
 
--- Test Stage.gd
+++ Test Stage.gd
@@ -1,15 +1,82 @@
 extends Node2D
 
+# vertical distance to annihilate the player. 
+export var kill_barrier = 300
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# block_distance to wait before generating a new block.
+export var block_distance_range = Vector2(200, 500)
+var block_distance = 200
 
+# display debug lines for the kill barriers.
+export var show_kill_barrier = false
 
-# Called when the node enters the scene tree for the first time.
+onready var rng = RandomNumberGenerator.new()
+onready var camera = $Camera2D
+onready var player = $Player
+onready var barrier = preload("res://kill-barrier.tscn")
+
+# distance the player has travelled without placing a new block.
+var distance = 0
+var previous_position = Vector2(0, 0)
+
+# debug_lines follow the kill barriers.
+# They cover the entire viewport width and move with the player. 
+var debug_lines = []
+
 func _ready():
-	pass
+	rng.randomize()
+	if show_kill_barrier:
+		create_debug_lines()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	process_kill_barriers()
+	update_debug_lines()
+	update_camera_position()
+	maybe_place_new_block()
+
+# maybe_place_new_block if we have travelled enough distance
+# since the last block.
+func maybe_place_new_block():
+	# compute the distance travelled between frames. 
+	distance += player.position.x - previous_position.x
+	previous_position = player.position
+
+	# maybe place block.
+	if distance > block_distance:
+		distance = 0
+		block_distance = rng.randf_range(block_distance_range.x, block_distance_range.y)
+		var b = barrier.instance()
+		b.position = Vector2(player.position.x + get_viewport().size.x, rng.randf_range(-300, 300))
+		b.scale = Vector2(rng.randf_range(0.5, 3), rng.randf_range(1, 5))
+		self.add_child(b)
+
+func create_debug_lines():
+	# hacky construction of line nodes... 
+	var vp = get_viewport()
+
+	var debug_line_top = Line2D.new()
+	debug_line_top.set_points([Vector2(-vp.size.x*2, -kill_barrier-1), Vector2(vp.size.x*2, -kill_barrier-1)])
+	debug_line_top.set_default_color(Color(100, 100, 100, 0.5))
+	debug_line_top.width = 1
+	debug_lines.append(debug_line_top)
+	self.add_child(debug_line_top)
+
+	var debug_line_bot = Line2D.new()
+	debug_line_bot.set_points([Vector2(-vp.size.x*2, kill_barrier), Vector2(vp.size.x*2, kill_barrier)])
+	debug_line_bot.set_default_color(Color(100, 100, 100, 0.5))
+	debug_line_bot.width = 1
+	debug_lines.append(debug_line_bot)
+	self.add_child(debug_line_bot)
+
+func update_debug_lines():
+	for line in debug_lines:
+		line.position.x = player.position.x
+
+# track the camera against the player. 
+func update_camera_position():
+	camera.position.x = player.position.x
+
+# reset scene when player wanders too far. 
+func process_kill_barriers():
+	if player.position.y >= kill_barrier or player.position.y <= -kill_barrier:
+		get_tree().reload_current_scene()
